f2b2005138c15b3514a0c08f89ae39f4b2f7cc67
AzhamDev <dev.azham@gmail.com>
2023-12-01 15:13:01 +0700

    feat(card list) fetch data and join event by id

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,30 @@
-"use client";
+import { CardEvent } from "@/components/event/components/CardEvent";
 
+async function getEvents() {
+  const res = await fetch("https://eventmakers-api.vercel.app/api/events", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  return data;
+}
 
-import { CardEvent } from '@/components/event/components/CardEvent';
-
-export default function Page() {
-
+export default async function Page() {
+  const { data } = await getEvents();
   return <main className='p-12 space-y-6'>
     <h1 className='text-3xl'>Active Event Now</h1>
-    <div >
-      <CardEvent />
+    <div className="grid grid-cols-4 gap-4">
+      {data.map(({ name, desc, date, id }) => (
+        <CardEvent
+          key={id}
+          name={name}
+          desc={desc}
+          date={date}
+          id={id}
+        />
+      ))}
     </div>
   </main>;
 }
