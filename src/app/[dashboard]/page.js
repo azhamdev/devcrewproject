ba2f42946f3d765b5027e6c9742f32d7e4a82efd
Dhafin <dhio@dhaf.in>
2023-12-01 14:32:16 +0700

    feat: add my active events list feature

--- src/app/[dashboard]/page.js
+++ src/app/[dashboard]/page.js
@@ -1,20 +1,9 @@
 "use client";
 
 import { Header } from "@/components/header";
+import { EventDashboardCard } from "@/components/EventDashboardCard";
 import { EventInput } from "@/components/eventinput";
-// import { EventDashboardCard } from "@/components/EventDashboardCard";
-
-async function getEventListByUserId(event) {
-  const res = await fetch(
-    "https://eventmakers-api.vercel.app/api/events?userid=2168fcb2-0609-4bec-bf4b-c1083daa0cdd",
-    { cache: "no-store" }
-  );
-  const data = await res.json();
-  return data;
-}
-
-export default async function Page() {
-  const { items } = await getEventListByUserId();
+export default function Page() {
   return (
     <div>
       <Header />
@@ -31,7 +20,7 @@ export default async function Page() {
           <div className="py-28 space-y-4">
             <div className="text-gray-500 text-xl ">My active events</div>
             <div className="grid grid-cols-2 lg:grid-cols-4 gap-3">
-              {/* <EventDashboardCard /> */}
+              <EventDashboardCard />
             </div>
           </div>
         </>
--- src/components/EventDashboardCard.jsx
+++ src/components/EventDashboardCard.jsx
@@ -1,60 +1,73 @@
-// import React from "react";
-// import { useRouter } from "next/navigation";
-// import {
-//   Card,
-//   CardHeader,
-//   CardBody,
-//   CardFooter,
-//   Divider,
-//   Image,
-// } from "@nextui-org/react";
+import React from "react";
+import { Button } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Image,
+} from "@nextui-org/react";
 
-// export const EventDashboardCard = ({ name, description, location, date }) => {
-//   const router = useRouter();
+import { useEffect, useState } from "react";
 
-//   const [currentName] = useState(name);
+export const EventDashboardCard = ({
+  name,
+  description,
+  date,
+  id,
+  location,
+}) => {
+  const [eventData, setEventData] = useState({});
 
-//   async function getEventByUserId() {
-//     const res = await fetch(
-//       "https://eventmakers-api.vercel.app/api/events?userid=2168fcb2-0609-4bec-bf4b-c1083daa0cdd",
-//       {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           name: name,
-//           description: description,
-//           location: location,
-//           date: date,
-//         }),
-//       }
-//     );
-//     const data = await res.json();
-//     console.log(data);
-//     router.refresh();
-//   }
-//   return (
-//     <Card className="min-w-[400px] ">
-//       <CardHeader className="flex gap-3">
-//         <Image
-//           alt="nextui logo"
-//           height={40}
-//           radius="sm"
-//           src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-//           width={40}
-//         />
-//         <div className="flex flex-col">
-//           <p className="text-md">{currentName}</p>
-//           <p className="text-small text-default-500">{location}</p>
-//         </div>
-//       </CardHeader>
-//       <Divider />
-//       <CardBody>
-//         <p>{description}</p>
-//       </CardBody>
-//       <Divider />
-//       <CardFooter className="text-sm text-gray-400">{date}</CardFooter>
-//     </Card>
-//   );
-// };
+  useEffect(() => {
+    getEventData();
+  }, []);
+
+  async function getEventData() {
+    const res = await fetch(
+      "https://eventmakers-api.vercel.app/api/events?userid=2168fcb2-0609-4bec-bf4b-c1083daa0cdd",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+    setEventData(data);
+  }
+
+  return (
+    <div className="space-y-3">
+      {eventData.data?.map(({ name, description, date, id }) => (
+        <Card key={id} className="min-w-[400px] ">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">{name}</p>
+              <p className="text-small text-default-500">{location}</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>{description}</p>
+          </CardBody>
+          <Divider />
+          <CardFooter className="flex justify-between text-sm text-gray-400">
+            <div>{date}</div>
+            <Button size="sm" className="bg-orange-500 text-white">
+              Participants
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+};
