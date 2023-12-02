a60c29de644a41260b160ca551d1dea43008c10e
Dhafin <dhio@dhaf.in>
2023-12-02 15:17:17 +0700

    add auth for dashbord

--- src/components/auth/hooks/useLogin.jsx
+++ src/components/auth/hooks/useLogin.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { API_URL } from "@/config/apiUrl";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 export const useLogin = () => {
   const router = useRouter();
@@ -30,9 +31,7 @@ export const useLogin = () => {
     });
     const data = await res.json();
     Cookies.set("token", data.token);
-    Cookies.set("userId", data.data.id);
-    localStorage.setItem("userdata", JSON.stringify(data.data));
-
+    
     if (!data) {
       setLoading(false);
       toast.error("Error Login!");
